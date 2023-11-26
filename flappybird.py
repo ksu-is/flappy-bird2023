2018e94ff91adb9ec6b11fb78305fd268fc402db
tcarver9 <142630595+tcarver9@users.noreply.github.com>
2023-11-26 09:10:00 -0500

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -36,11 +36,31 @@ if __name__ == "main":
     pygame.image.load('images/0.png').convert_alpha(),
     
 game_images ('flappy bird') = pygame.image.load(birdplayer_image).convert_alpha()
-game_images ('sealevel') = pygame.image.load(sealevel_image).convert_alpha()
+game_images ('sea_level') = pygame.image.load(sealevel_image).convert_alpha()
 game_images ('background') = pygame.image.load(background_image).convert_alpha()
 game_images ('pipeimage') = pygame.transform.rotate(pygame.image.load(pipeimage).convert_alpha(),180),pygame.image.load(pipeimage).convert_alpha())
 print("Welcome back to Flappy Bird!")
 print("Press enter to start the game.")
+#establishing the bird position + world loop
+horizontal = int(window_width/4.75)
+vertical = int((window_height - game_images['flappybird'].get_height())/2
+ground = 0
+      while True:
+        for event in pygame.event.get():
+            #close game function
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                sys.exit() 
+#start game with enter or space bar
+        elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_ENTER):flappygame()
+#game wont start if player doesnt press anything
+        else:
+        window.blit(game_images['background'], (0,0))
+        window.blit(game_images['flappybird'], (horizontal, vertical))  
+        window.blit(game_images['sealevel'], (ground, elevation))
+        #refresh
+        pygame.display.update()
+        #show fps
+        framepersecond_clock.tick(framepersecond)
 
 
 
