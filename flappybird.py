876b06b2d389efe186de625cb2b8277a51e51f2c
tcarver9 <142630595+tcarver9@users.noreply.github.com>
2023-12-04 15:10:52 -0500

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -128,7 +128,62 @@ def flappygame():
                 if vertical > 0: 
                     bird_velocity_y = bird_flap_velocity 
                     bird_flapped = True
-                  
+#this function will return true if the flappybird is crashed 
+        game_over = isGameOver(horizontal, vertical, up_pipes, down_pipes) 
+        if game_over: 
+            return
+        # check for your_score 
+        playerMidPos = horizontal + game_images['flappybird'].get_width()/2
+        for pipe in up_pipes: 
+            pipeMidPos = pipe['x'] + game_images['pipeimage'][0].get_width()/2
+            if pipeMidPos <= playerMidPos < pipeMidPos + 4: 
+                  # Printing the score 
+                your_score += 1
+                print(f"Your your_score is {your_score}") 
+        if bird_velocity_y < bird_Max_Vel_Y and not bird_flapped: 
+            bird_velocity_y += birdAccY 
+        if bird_flapped: 
+            bird_flapped = False
+        playerHeight = game_images['flappybird'].get_height() 
+        vertical = vertical + min(bird_velocity_y, elevation - vertical - playerHeight) 
+        #move pipes to the left 
+        for upperPipe, lowerPipe in zip(up_pipes, down_pipes): 
+            upperPipe['x'] += pipeVelX 
+            lowerPipe['x'] += pipeVelX 
+  
+        #add a new pipe when the first is about to cross the screen 
+        if 0 < up_pipes[0]['x'] < 5: 
+            newpipe = createPipe() 
+            up_pipes.append(newpipe[0]) 
+            down_pipes.append(newpipe[1]) 
+        #if the pipe is out of the screen, remove it 
+        if up_pipes[0]['x'] < -game_images['pipeimage'][0].get_width(): 
+            up_pipes.pop(0) 
+            down_pipes.pop(0) 
+        #blit the game images now 
+        window.blit(game_images['background'], (0, 0)) 
+        for upperPipe, lowerPipe in zip(up_pipes, down_pipes): 
+            window.blit(game_images['pipeimage'][0], 
+                        (upperPipe['x'], upperPipe['y'])) 
+            window.blit(game_images['pipeimage'][1], 
+                        (lowerPipe['x'], lowerPipe['y'])) 
+        window.blit(game_images['sea_level'], (ground, elevation)) 
+        window.blit(game_images['flappybird'], (horizontal, vertical)) 
+        #fetching the score. 
+        numbers = [int(x) for x in list(str(your_score))] 
+        width = 0
+        #finding the width of score images from numbers. 
+        for num in numbers: 
+            width += game_images['scoreimages'][num].get_width() 
+        Xoffset = (window_width - width)/1.1
+        #blitting the images on the window. 
+        for num in numbers: 
+            window.blit(game_images['scoreimages'][num], (Xoffset, window_width*0.02)) 
+            Xoffset += game_images['scoreimages'][num].get_width() 
+        #refreshing the game window and displaying the score. 
+        pygame.display.update() 
+        #set framepersecond 
+        framepersecond_clock.tick(framepersecond)
 
 
         
